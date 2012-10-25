36ec1fbd73a5c23a3d724aee696951dc83ee2683
Jonathan Keane <jkeane@gmail.com>
2012-10-25 13:41:37 -0500

    Added audio, video filters, and video codec options. I've given sensble defaults, so this shouldn't break anything, but this should be more standardized.

--- clipper.py
+++ clipper.py
@@ -7,7 +7,7 @@ from datetime import timedelta #both datetime imports needed?
 
 class clipper:
     """Generates the actually ffmpeg clipping"""
-    def __init__(self, annos, outPath, inFile):
+    def __init__(self, annos, outPath, inFile, audio, videoFilters, videoCodec):
         tstart = float(annos[0])/1000. # convert milliseconds to seconds
         tend = float(annos[1])/1000. # convert milliseconds to seconds
         outFile = quote(outPath.replace( " ", r"" )+".mp4") # strips spaces, there has to be a better way than this.
@@ -16,18 +16,22 @@ class clipper:
         #logfile = quote(''.join([flag,outName,"-",name,"-clip",str(n),".log"]))
         #infile = quote(path)
 
-        proc = self.clipFunc(infile=inFile, outfile=outFile, tstart=tstart, tend=tend, log=None, verbose=True)
+        proc = self.clipFunc(infile=inFile, outfile=outFile, tstart=tstart, tend=tend, audio=audio, videoFilters=videoFilters, videoCodec=videoCodec,  log=None, verbose=True)
         self.subProc = proc
     
-    def clipFunc(self, infile, outfile, tstart, tend, log=None, verbose=False):
-        ''' clipps video with no options '''
+    def clipFunc(self, infile, outfile, tstart, tend, audio='', videoFilters='', videoCodec='mpeg4', log=None, verbose=False):
+        ''' clips video with no options '''
         #deinterlace+crop+scale '-vf "[in] yadif=1 [o1]; [o1] crop=1464:825:324:251 [o2]; [o2] scale=852:480 [out]"'
         #deinterlace+crop '-vf "[in] yadif=1 [o1]; [o1] crop=1464:825:324:251 [out]"'
         #deinterlace '-vf "[in] yadif=1 [out]"'
         dur = tend-tstart
         cmd = ['../Resources/ffmpeg']
         cmd = ['ffmpeg']
-        opts = ['-i', infile, '-ss', str(tstart), '-t', str(dur),'-sameq', '-y', outfile]
+        if audio == False:
+            aud = '-an'
+        else:
+            aud = ''
+        opts = ['-i', infile, '-vf', videoFilters, '-ss', str(tstart), '-t', str(dur),'-sameq', aud, '-vcodec', videoCodec, '-y', outfile]
         cmd.extend(opts)
         if verbose: print(cmd)
         if log: logFile = open(log, 'w')
